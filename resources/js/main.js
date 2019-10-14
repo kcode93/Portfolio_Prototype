a33c73b6f719484dc6897ebd2033622039c39419
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-10-14 09:59:44 -0700

    added the showMenu variable to control menu's state, also generated an event listener on the menu button and created the toggleMenu()

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,17 @@
+//set Initial State of Menu
+let showMenu = false;
+
 //selections
 const btnMenu = document.querySelector('.btn-menu');
 const menu = document.querySelector('.menu');
 const menuBrand = document.querySelector('.menu-brand');
 const menuNav = document.querySelector('.btn-nav');
 const navItem = document.querySelectorAll('.nav-item');
+
+//event listeners
+btnMenu.addEventListener('click', toggleMenu);
+
+//functions
+fucntion toggleMenu(){
+    
+}
