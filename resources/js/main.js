dc4fa4c6f6b934c6d7f571880b52e6e583d6f249
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-10-14 09:55:57 -0700

    added the main.js file to the resources folder also did all the needed query selections for DOM manipulation

---
+++ resources/js/main.js
@@ -0,0 +1,6 @@
+//selections
+const btnMenu = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuBrand = document.querySelector('.menu-brand');
+const menuNav = document.querySelector('.btn-nav');
+const navItem = document.querySelectorAll('.nav-item');
